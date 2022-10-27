9964f0af11ff911c044255906a259c01a1747b74
Tamajin <wild.jlq@gmail.com>
2022-10-27 10:43:58 +0200

    creation de deux nouveaux composants pour envoie de data de l'enfant (magic-oven) vers le parent (kitchen)

---
+++ src/app/magic-oven/magic-oven.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-magic-oven',
+  templateUrl: './magic-oven.component.html',
+  styleUrls: ['./magic-oven.component.css']
+})
+export class MagicOvenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
